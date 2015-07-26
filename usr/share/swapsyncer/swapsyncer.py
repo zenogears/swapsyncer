c8c644ac66bf5250dd84bc2d61bcb87c6edc9c36
zenogears <zenogears.aka@gmail.com>
2015-07-26 03:17:29 +0300

    some updates with pythonzenity

--- usr/share/swapsyncer/swapsyncer.py
+++ usr/share/swapsyncer/swapsyncer.py
@@ -9,48 +9,48 @@ import pythonzenity
 import getpass
 
 from datetime import datetime
+from decimal import Decimal
 
 from pythonzenity import Question
 from pythonzenity import Warning
 from pythonzenity import GetDirectory
 from pythonzenity import InfoMessage
+from pythonzenity import TextInfo
 from pythonzenity import Progress
 
+##Site
 url = "http://swapbase.ru/"
-username = getpass.getuser()
-conf_dir_path = "/home/" + username + "/.swapsyncer"
+
+conf_dir_path = "/home/" + getpass.getuser() + "/.swapsyncer"
 conf_path = conf_dir_path + "/config.ini"
 log_path = conf_dir_path + "/swapsyncer.log"
+
 dir_path = os.path.abspath(os.path.dirname(sys.argv[0])) 
-eboots_path = dir_path + "/SWAP"
-eboots_path = eboots_path.decode('cp1251')
 
 now = datetime.now()
 date = datetime.strftime(datetime.now(), "%Y.%m.%d %H:%M:%S")
 
+## Create conf dir if not exist
 if not  os.path.exists(conf_dir_path):
   os.makedirs(conf_dir_path)
-  
-lp = open(log_path, 'a+')
 
+## create log and conf files if not exist  
+lp = open(log_path, 'a+')
 fp = open(conf_path, 'a+')
 
+## create conf-file
 if not fp.read():
-  if (Question(text="Директория SWAP не определена. Хотите найти её?")):
+  if (Question(text="Конфиг не найден.\nДиректория SWAP не определена.\nХотите указать директорию под SWAP?")):
     swapdir = GetDirectory(sep=None)
     fp.write(str(swapdir)[2:-2])
     fp.close()
 
-
-if not lp.read():
-  lp.write(str(date))
-  lp.close()
-
-
+#if not lp.read():
+#  lp.write(str(date))
+#  lp.close()
 
 fp = open(conf_path)
 lp = open(log_path, 'w+')
-lp.write(str(date) + "\n")
 
 eboots_path = str(fp.read())
    
@@ -65,30 +65,47 @@ else:
 	upload_list = list(set(local_eboots) - set(remote_eboots))
 	
 	if len(download_list)==0:
-		InfoMessage(text="Нечего скачивать")
+		InfoMessage(text="Нечего скачивать\nКоллекция обновлена")
 	else:
-		f_to_download = str(len(download_list)) + " файлов скачано"
-		InfoMessage(text=f_to_download)
+		lp.write(str(date) + "\n")
+		InfoMessage(text=str(len(download_list)) + " файлов будет скачано")
+		testparam = float(100/float(len(download_list)))
+		update = Progress(text='Downloading...', percentage=0,width=470,auto_close=True)
 	i = 1;
+	palka = 0;
 	
 	for fn in download_list:
 		lp.write("D[" + str(i) + "/" + str(len(download_list)) + "]" + fn + "\n")
 		i+=1
+		palka = float(palka) + float(testparam)
+		datchik = int(palka)
+		update(datchik,"Обновляем базу, подождите... Это может занять длительное время. Скачано: " + str("%.2f" % palka) + " %")
 		r = requests.get(url + "/SWAP/" + fn)
 		with open(eboots_path + '/' + fn, 'wb') as fd:
 			fd.write(r.content)
 			fd.close()
-	if len(download_list)!=0:
-		InfoMessage(text=str(len(upload_list)) + " файлов скачано")
-		
+	if not len(download_list)==0:
+		update(100)
 	if len(upload_list)==0:
-		InfoMessage(text="Нечего загружать")
+		InfoMessage(text="Нечего загружать\nНичего нового")
 	else:
-		InfoMessage(text=str(len(upload_list)) + " files to upload")
+		InfoMessage(text=str(len(upload_list)) + " файлов будет загружено")
+		testparam = float(100/float(len(upload_list)))
+		update = Progress(text='Uploading...', percentage=0, width=470, auto_close=True)
+		lp.write(str(date) + "\n")
 	i = 1;
+	palka = 0;
 	for fn in upload_list:
 		lp.write ("U[" + str(i) + "/" + str(len(upload_list)) + "]" + fn + "\n")
 		i+=1
+		palka = float(palka) + float(testparam)
+		datchik = int(palka)
+		update(datchik,"Загружаем данные на сервер, подождите... Это может занять длительное время. Загружено: " + str("%.2f" % palka) + " %")
 		r = requests.post(url + "/uploadify.php", files={'Filedata': open(eboots_path + '/' + fn, 'rb')})
-	if len(upload_list)!=0:
-		InfoMessage(text=str(len(upload_list)) + " files uploaded")
+	if not len(upload_list)==0:
+		update(100)
+	lp.close()
+
+	if len(upload_list)!=0 or len(download_list)!=0:
+	  if (Question(text="Желаете посмотреть изменения?")):
+	    TextInfo(filename=log_path, editable=False, width=400, height=200)
